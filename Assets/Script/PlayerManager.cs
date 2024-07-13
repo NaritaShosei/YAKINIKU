e6b986186e861f795e904d1b898202d5586f231d
NaritaShosei <onigirikun7171@gmail.com>
2024-07-13 18:08:32 +0900

    SmokeとDamage

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
     [SerializeField] GameObject[] _moveTargetPos;
+    [SerializeField] float _hp;
+    bool _smokeStart;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Damage();
     }
     void Move()
     {
@@ -34,6 +37,27 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position = _moveTargetPos[3].transform.position;
+        }
+    }
+    void Damage()
+    {
+        if (_smokeStart)
+        {
+            _hp -= Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Smoke")
+        {
+            _smokeStart = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Smoke")
+        {
+            _smokeStart = false;
         }
     }
 }
--- Assets/Script/SmokeGenerator.cs
+++ Assets/Script/SmokeGenerator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SmokeGenerator : MonoBehaviour
 {
+    [SerializeField] float _waitTime;
+    [SerializeField] Vector3[] _direction;
+    int _randomIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(StartSmokeDirectionChange());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator StartSmokeDirectionChange()
+    {
+        yield return new WaitForSeconds(_waitTime);
+        _randomIndex = Random.Range(0, _direction.Length);
+        transform.localRotation = Quaternion.Euler(_direction[_randomIndex]);
+        StartCoroutine(StartSmokeDirectionChange());
     }
 }
