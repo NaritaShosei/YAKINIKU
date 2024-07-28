5b4cbe0721d719f458de80a4f40c76d81473ad44
NaritaShosei <onigirikun7171@gmail.com>
2024-07-28 17:11:21 +0900

    焼肉のスタートを設定

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] _moveTargetPos;
+    [SerializeField] Vector2[] _moveTargetPos;
     [SerializeField] float _hp;
-    bool _smokeStart;
+    [SerializeField] float _damageSize = 1;
+    bool _smokeCollision;
 
     // Start is called before the first frame update
     void Start()
@@ -24,40 +25,40 @@ public class PlayerManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            transform.position = _moveTargetPos[0].transform.position;
+            transform.position = _moveTargetPos[0];
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.position = _moveTargetPos[1].transform.position;
+            transform.position = _moveTargetPos[1];
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            transform.position = _moveTargetPos[2].transform.position;
+            transform.position = _moveTargetPos[2];
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = _moveTargetPos[3].transform.position;
+            transform.position = _moveTargetPos[3];
         }
     }
     void Damage()
     {
-        if (_smokeStart)
+        if (_smokeCollision)
         {
-            _hp -= Time.deltaTime;
+            _hp -= Time.deltaTime * _damageSize;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Smoke")
         {
-            _smokeStart = true;
+            _smokeCollision = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Smoke")
         {
-            _smokeStart = false;
+            _smokeCollision = false;
         }
     }
 }
--- Assets/Script/SmokeGenerator.cs
+++ Assets/Script/SmokeGenerator.cs
@@ -7,6 +7,7 @@ public class SmokeGenerator : MonoBehaviour
     [SerializeField] float _waitTime;
     [SerializeField] Vector3[] _direction;
     int _randomIndex;
+    bool _yakinikuStart;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,17 @@ public class SmokeGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        YakinikuStart();
+    }
+    void YakinikuStart()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && !_yakinikuStart)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            _randomIndex = Random.Range(0, _direction.Length);
+            transform.localRotation = Quaternion.Euler(_direction[_randomIndex]);
+            _yakinikuStart = true;
+        }
     }
     IEnumerator StartSmokeDirectionChange()
     {
